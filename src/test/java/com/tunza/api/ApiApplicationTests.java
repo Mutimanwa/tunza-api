6ba6c317fec5c67eb26e6de7dce15cfdba5fad22
Mutimanwa <calvinmutimanwa9@gmail.com>
2025-12-23 09:22:27 +0200

    chore(api): move spring boot project to repo root

---
+++ src/test/java/com/tunza/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunza.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
